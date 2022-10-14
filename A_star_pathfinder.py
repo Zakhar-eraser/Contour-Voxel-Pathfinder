8bcc6e8691834e4708f672ab055614090bbb23eb
Zakhar-eraser <minecraft177@yandex.ru>
2022-10-14 15:14:56 +0300

    something wrong with neighbours finding

--- A_star_pathfinder.py
+++ A_star_pathfinder.py
@@ -2,71 +2,56 @@ import numpy as np
 from queue import PriorityQueue
 
 voxel_step_cost = 1.0
-obstacle_cost = 0.0
-obstacle_check_depth = 2
+obstacle_cost = 1.0
+obstacle_check_depth = 1
 
-class VoxelNode:
-    """An object of the class contains voxel`s indexes and cost of movement to the voxel"""
+class Node:
+    """Interface to work numpy array with PriorityQueue"""
 
-    def __init__(self, idx, parent):
+    def __init__(self, idx, priority):
         self.idx = idx
-        self.parent = parent
-        self.cost = 0.0
-        self.nbrs = list()
+        self.priority = priority
+
+    def __eq__(self, other):
+        return self.priority == other.priority
     
-    def __eq__(self, __o: object) -> bool:
-        return np.array_equal(self.idx, __o.idx)
-    
-    def __hash__(self) -> int:
+    def __hash__(self):
         return hash(tuple(self.idx))
 
     def __lt__(self, other):
-        return self.cost < other.cost
-
-def index_in_bounds(grid, idx):
-    bounds = grid.shape
-    return ((idx[0] < bounds[0]) and (idx[1] < bounds[1]) and (idx[2] < bounds[2])  and (
-        (idx[0] >= 0) and (idx[1] >= 0) and (idx[2] >= 0))
-        and not grid[idx[0], idx[1], idx[2]])
-
-def local_free_grid(grid, idx):
-    local_grid = np.zeros((3, 3, 3), np.int32)
-    for x in range(-1, 2):
-        for y in range(-1, 2):
-            for z in range(-1, 2):
-                if index_in_bounds(grid, idx + np.array((x, y, z), np.int32)):
-                    local_grid[x, y, z] = 1
-    return local_grid
+        return self.priority < other.priority
 
 def get_neighbours_occupancy(local_grid):
+    #print(local_grid)
     for order in range(3):
         for i in (-1, 1):
             for j in (-1, 1):
-                n1 = np.roll(np.array((i, 0, 0), np.int32), order)
-                n2 = np.roll(np.array((0, j, 0), np.int32), order)
-                if (not local_grid[tuple(n1)]) or (not local_grid[tuple(n2)]):
-                    local_grid[tuple(n1 + n2)] = 0
+                n1 = np.roll((i, 0, 0), order)
+                n2 = np.roll((0, j, 0), order)
+                if local_grid[tuple(n1)] or local_grid[tuple(n2)]:
+                    local_grid[tuple(n1 + n2)] = 1
 
     for i in (-1, 1):
         for j in (-1, 1):
             for k in (-1, 1):
-                n1 = np.array((i, 0, 0), np.int32)
-                n2 = np.array((0, j, 0), np.int32)
-                n3 = np.array((0, 0, k), np.int32)
-                if (not local_grid[tuple(n1)]) or (not local_grid[tuple(n2)]) or (
-                    not local_grid[tuple(n3)]
+                n1 = np.array((i, 0, 0), np.int8)
+                n2 = np.array((0, j, 0), np.int8)
+                n3 = np.array((0, 0, k), np.int8)
+                if local_grid[tuple(n1)] or local_grid[tuple(n2)] or (
+                    local_grid[tuple(n3)]
                     ):
-                    local_grid[tuple(n1 + n2 + n3)] = 0
+                    local_grid[tuple(n1 + n2 + n3)] = 1
     return local_grid
 
 def get_neighbours(grid, idx):
-    nbrs_grid = get_neighbours_occupancy(local_free_grid(grid, idx))
+    nbrs_grid = get_neighbours_occupancy(np.copy(
+        grid[idx[0] - 1:idx[0] + 2, idx[1] - 1:idx[1] + 2, idx[2] - 1:idx[2] + 2]))
     nbrs = list()
     for x in range(-1, 2):
         for y in range(-1, 2):
             for z in range(-1, 2):
                 local_idx = (x, y, z)
-                if nbrs_grid[local_idx]:
+                if not nbrs_grid[local_idx]:
                     nbrs.append(np.array(local_idx) + idx)
     return nbrs
 
@@ -84,41 +69,64 @@ def heuristics(cur, end):
 def obstacle_closeness(grid, idx):
     for depth in range(1, obstacle_check_depth + 1):
         for shift in range(0, 3):
-            for i in range(-depth + 1, depth - 1):
-                for j in range(-depth + 1, depth - 1):
+            for i in range(-depth + 1, depth):
+                for j in range(-depth + 1, depth):
                     for k in (-depth, depth):
-                        if index_in_bounds(grid)
+                        if grid[tuple(idx + np.roll((i, j, k), shift))]:
+                            return depth
 
-def get_cost(grid, idx):
+            for i in (-depth, depth):
+                for j in range(-depth + 1, depth):
+                    for k in (-depth, depth):
+                        if grid[tuple(idx + np.roll((i, j, k), shift))]:
+                            return depth
+            
+            for i in (-depth, depth):
+                for j in (-depth, depth):
+                    for k in (-depth, depth):
+                        if grid[tuple(idx + np.roll((i, j, k), shift))]:
+                            return depth
 
+    return obstacle_check_depth
+
+def get_cost(grid, current, next):
+    dist_cost = manh_dist(current, next)
+    dist_cost = 1 if dist_cost == 1 else 1.4 if dist_cost == 2 else 1.7
+
+    obst_cost = obstacle_cost / obstacle_closeness(grid, next)
+
+    cost = dist_cost + obst_cost
+    return cost
 
 def find_path_A_star(grid, start, end, stop_condition):
     frontier = PriorityQueue()
-    frontier.put((0, start))
-    costs = {start: 0.0}
-    parents = {start: None}
+    frontier.put((0, Node(start, 0)))
+    costs = {tuple(start): 0.0}
+    parents = {tuple(start): None}
 
     while not frontier.empty():
-        current = frontier.get()[1]
+        current = frontier.get()[1].idx
 
         if stop_condition(current, end):
             break
-      
-        for next in get_neighbours(current):
-            new_cost = costs[current] + get_cost(next)
-            if (next not in costs) or (new_cost < costs[next]):
-                costs[next] = new_cost
-                priority = new_cost + heuristics(next, end)
-                frontier.put((priority, next))
+        
+        nbrs = get_neighbours(grid, current)
+        for next in nbrs:
+            new_cost = costs[tuple(current)] + get_cost(grid, current, next)
+            if (tuple(next) not in costs) or (new_cost < costs[tuple(next)]):
+                parents[tuple(current)] = next
+                costs[tuple(next)] = new_cost
+                next_node = Node(next, new_cost + heuristics(next, end))
+                frontier.put((next_node.priority, next_node))
     
-    return costs
+    return parents
 
 def get_route_idx(graph, end):
     cur = end
     route = list()
-    while graph[cur] is not None:
+    while graph[tuple(cur)] is not None:
         route.append(cur)
-        cur = graph[cur]
+        cur = graph[tuple(cur)]
     route.append(cur)
-    return np.array(route, dtype=np.int32)
+    return np.array(route, dtype=np.int32) - 1
    
--- contour_pathfind.py
+++ contour_pathfind.py
@@ -14,20 +14,24 @@ def pick_points(pcd):
     vis.destroy_window()
     return vis.get_picked_points()
 
-def get_max_bounding_idx(pcd):
+def get_max_shape_idx(pcd):
     max_bound = pcd.get_max_bound()
     min_bound = pcd.get_min_bound()
-    max_idx = (max_bound - min_bound) / voxel_size + np.ones(3, dtype=np.int32)
-    max_idx = max_idx.astype('int32')
-    return max_idx
+    shape = (max_bound - min_bound) / voxel_size + np.ones(3, dtype=np.int32)
+    shape = shape.astype('int32')
+    return shape
 
-def get_occupancy_grid(boundary, voxels):
-    grid = np.zeros(tuple(boundary + 2), dtype=np.int8)
+def get_occupancy_grid(shape, voxels):
+    grid = np.zeros(tuple(shape + 2), dtype=np.int8)
     grid[:, :, 0] = 1
-    grid[:, :, boundary[2] + 1] = 1
+    grid[:, :, shape[2] + 1] = 1
+    grid[0, :, 1:shape[2] + 1] = 1
+    grid[shape[0] + 1, :, 1:shape[2] + 1] = 1
+    grid[1:shape[0] + 1, 0, 1:shape[2] + 1] = 1
+    grid[1:shape[0] + 1, shape[1] + 1, 1:shape[2] + 1] = 1
     
     for vox in voxels:
-        grid[vox.grid_index[0], vox.grid_index[1], vox.grid_index[2]] = 1
+        grid[tuple(vox.grid_index + 1)] = 1
     return grid
 
 def points_sqr_len(p1, p2):
@@ -84,7 +88,7 @@ def check_intersection(start_voxel, target_voxel, occupancy_grid):
     return intersect
 
 def same_point_condition(current_point, target_point):
-    return current_point == target_point
+    return np.array_equal(current_point, target_point)
 
 def point_in_range_condition(current_point, target_point):
     return max_observe_dist > points_sqr_len(current_point, target_point) * voxel_size
@@ -102,24 +106,26 @@ def draw_route(voxel_grid, route, min_bound):
 
 def main():
     pcd = o3d.io.read_point_cloud(input_path)
-    #print("Yellow sphere is the start point, Blue sphere is the target point")
-    #mission_points_ids = list(set(pick_points(pcd)))
-    #assert (len(mission_points_ids) == 2), "It must be exactly 2 picked points"
+    print("Yellow sphere is the start point, Blue sphere is the target point")
+    mission_points_ids = list(set(pick_points(pcd)))
+    assert (len(mission_points_ids) == 2), "It must be exactly 2 picked points"
 
-    #target_point = np.asarray(pcd.points)[mission_points_ids[0]]
-    #start_point = np.asarray(pcd.points)[mission_points_ids[1]]
+    target_point = np.asarray(pcd.points)[mission_points_ids[0]]
+    start_point = np.asarray(pcd.points)[mission_points_ids[1]]
 
     pcd = pcd.voxel_down_sample(voxel_size)
     voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd, voxel_size)
 
-    #target_voxel = voxel_grid.get_voxel(target_point)
-    #start_voxel = voxel_grid.get_voxel(start_point)
-    start_voxel = voxel_grid.get_voxel(np.array((-19, 17, 15))) ## test
-    target_voxel = voxel_grid.get_voxel(np.array((31, -13, 79))) ## test
+    target_voxel = voxel_grid.get_voxel(target_point)
+    start_voxel = voxel_grid.get_voxel(start_point)
+    ##start_voxel = voxel_grid.get_voxel(np.array((-19, 17, 15))) ## test
+    ##target_voxel = voxel_grid.get_voxel(np.array((31, -13, 79))) ## test
 
+    start_voxel += 1
+    target_voxel += 1
     voxels = voxel_grid.get_voxels()
-    boundary = get_max_bounding_idx(pcd)
-    occupancy_grid = get_occupancy_grid(boundary, voxels)
+    shape = get_max_shape_idx(pcd)
+    occupancy_grid = get_occupancy_grid(shape, voxels)
     occupancy_grid[tuple(target_voxel)] = 0
     occupancy_grid[tuple(start_voxel)] = 0
 

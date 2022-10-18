fb086473e9153fe0671c12b6907bda3899d1e0b6
Zakhar-eraser <minecraft177@yandex.ru>
2022-10-18 21:07:57 +0300

    done but invalid intersection function

--- voxel_raycast.py
+++ voxel_raycast.py
@@ -6,14 +6,87 @@ def sqr_dist(v1, v2):
     v = v1 - v2
     return np.sum(np.square(v))
 
-def check_intersection1(start_voxel, target_voxel, occupancy_grid):
+def zStep(pos, steps, start_pos, bnds, ds):
+    pos[2] += steps[2]
+
+def yStep(pos, steps, start_pos, bnds, ds):
+    pos[1] += steps[1]
+
+def xStep(pos, steps, start_pos, bnds, ds):
+    pos[0] += steps[0]
+
+def xyStep(pos, steps, start_pos, bnds, dds):
+    x_by_y = dds[0] * (bnds[1] - start_pos[1])
+    y_by_x = dds[1] * (bnds[0] - start_pos[0])
+    distXZ = sqr_dist((bnds[0], y_by_x, start_pos[2]), pos)
+    distYZ = sqr_dist((x_by_y, bnds[1], start_pos[2]), pos)
+    if distXZ == distYZ:
+        pos += (steps[0], steps[1], 0)
+    elif distXZ < distYZ:
+        pos[0] += steps[0]
+    else:
+        pos[1] += steps[1]
+
+def yzStep(pos, steps, start_pos, bnds, dds):
+    y_by_z = dds[0] * (bnds[2] - start_pos[2])
+    distXY = sqr_dist((start_pos[0], y_by_z, bnds[2]), pos)
+    z_by_y = dds[1] * (bnds[1] - start_pos[1])
+    distYZ = sqr_dist((start_pos[0], bnds[1], z_by_y), pos)
+
+    if distYZ == distXY:
+        pos += (0, steps[1], steps[2])
+    elif distYZ < distXY:
+        pos[1] += steps[1]
+    else:
+        pos[2] += steps[2]
+
+def xzStep(pos, steps, start_pos, bnds, dds):
+    x_by_z = dds[0] * (bnds[2] - start_pos[2])
+    z_by_x = dds[1] * (bnds[0] - start_pos[0])
+    distXZ = sqr_dist((bnds[0], start_pos[1], z_by_x), pos)
+    distXY = sqr_dist((x_by_z, start_pos[1], bnds[2]), pos)
+
+    if distXZ == distXY:
+        pos += (steps[0], 0, steps[2])
+    elif distXZ < distXY:
+        pos[0] += steps[0]
+    else:
+        pos[2] += steps[2]
+
+def xyzStep(pos, steps, start_pos, bnds, dds):
+    x_by_z = dds[0] * (bnds[2] - start_pos[2])
+    y_by_z = dds[1] * (bnds[2] - start_pos[2])
+    z_by_x = dds[2] * (bnds[0] - start_pos[0])
+    distXY = sqr_dist((x_by_z, y_by_z, bnds[2]), pos)
+    distYZ = sqr_dist((bnds[0], y_by_z, z_by_x), pos)
+    distXZ = sqr_dist((x_by_z, bnds[1], z_by_x), pos)
+
+    if (distXY == distYZ) and (distXY == distXZ):
+        pos += steps
+    elif distXY == distYZ:
+        if distXY < distXZ:
+            pos += (0, steps[1], steps[2])
+        else: pos[0] += steps[0]
+    elif distYZ == distXZ:
+        if distYZ < distXY:
+            pos += (steps[0], steps[1], 0)
+        else: pos[2] += steps[2]
+    elif distXZ == distXY:
+        if distXZ < distYZ:
+            pos += (steps[0], 0, steps[2])
+        else: pos[1] += steps[1]
+    elif (distXY < distYZ) and (distXY < distXZ):
+        pos[2] += steps[2]
+    elif (distYZ < distXY) and (distYZ < distXZ):
+        pos[1] += steps[1]
+    else:
+        pos[0] += steps[0]
+
+def check_intersection(start_voxel, target_voxel, occupancy_grid):
     tmp = occupancy_grid[tuple(target_voxel)]
     occupancy_grid[tuple(target_voxel)] = 0
     intersect = False
-    cur_voxel = start_voxel
-    x1 = start_voxel[0]
-    y1 = start_voxel[1]
-    z1 = start_voxel[2]
+    cur_voxel = np.copy(start_voxel)
     dX = target_voxel[0] - start_voxel[0]
     dY = target_voxel[1] - start_voxel[1]
     dZ = target_voxel[2] - start_voxel[2]
@@ -25,58 +98,31 @@ def check_intersection1(start_voxel, target_voxel, occupancy_grid):
     sX = 1 if dX > 0 else -1
     sY = 1 if dY > 0 else -1
     sZ = 1 if dZ > 0 else -1
-
-    while intersect == False and not np.array_equal(cur_voxel, target_voxel):
-        bnds = cur_voxel + B
-        if dX == 0 and dY == 0:
-            cur_voxel[2] += sZ
-        elif dX == 0 and dZ == 0:
-            cur_voxel[1] += sY
-        elif dY == 0 and dZ == 0:
-            cur_voxel[0] += sX
-        elif dX == 0:
-            y_by_z = dY/dZ * (bnds[2] - z1)
-            distXY = sqr_dist((x1, y_by_z, bnds[2]), cur_voxel)
-            z_by_y = dZ/dY * (bnds[1] - y1)
-            distYZ = sqr_dist((x1, bnds[1], z_by_y), cur_voxel)
-            if distYZ == distXY:
-                cur_voxel += (0, sY, sZ)
-            elif distYZ < distXY:
-                cur_voxel[1] += sY
-            else:
-                cur_voxel[2] += sZ
-        elif dY == 0:
-            x_by_z = dY/dZ * (bnds[2] - z1)
-            z_by_x = dZ/dX * (bnds[0] - x1)
-            distXZ = sqr_dist((bnds[0], y1, z_by_x), cur_voxel)
-            distXY = sqr_dist((x_by_z, y1, bnds[2]))
-            if distXZ == distXY:
-                cur_voxel += (sX, 0, sZ)
-            elif distXZ < distXY:
-                cur_voxel[0] += sX
-            else:
-                cur_voxel[2] += sZ
-        elif dZ == 0:
-            
-            
-        #x_by_y = dXdY * (bnds[1] - y1)
-        #x_by_z = dXdZ * (bnds[2] - z1)
-        #y_by_x = dYdX * (bnds[0] - x1)
-        #y_by_z = dYdZ * (bnds[2] - z1)
-        #z_by_x = dZdX * (bnds[0] - x1)
-        #z_by_y = dZdY * (bnds[1] - y1)
-
-        distXY = sqr_dist([x_by_z, y_by_z, bnds[2]], cur_voxel)
-        distYZ = sqr_dist([bnds[0], y_by_x, z_by_x], cur_voxel)
-        distXZ = sqr_dist([x_by_y, bnds[1], z_by_y], cur_voxel)
-
-        if (distXY <= distXZ) and (distXY <= distYZ):
-            cur_voxel[2] += sZ
-        if (distXZ <= distXY) and (distXZ <= distYZ):
-            cur_voxel[1] += sY
-        if (distYZ <= distXZ) and (distYZ <= distXY):
-            cur_voxel[0] += sX
+    steps = (sX, sY, sZ)
+    if dX == 0 and dY == 0:
+        step_fun = zStep
+        dds = None
+    elif dX == 0 and dZ == 0:
+        step_fun = yStep
+        dds = None
+    elif dY == 0 and dZ == 0:
+        step_fun = xStep
+        dds = None
+    elif dX == 0:
+        step_fun = yzStep
+        dds = (dY/dZ, dZ/dY)
+    elif dY == 0:
+        step_fun = xzStep
+        dds = (dX/dZ, dZ/dX)
+    elif dZ == 0:
+        step_fun = xyStep
+        dds = (dX/dY, dY/dX)
+    else:
+        step_fun = xyzStep
+        dds = (dX/dZ, dY/dZ, dZ/dX)
         
+    while (intersect == False) and (not np.array_equal(cur_voxel, target_voxel)):
+        step_fun(cur_voxel, steps, start_voxel, cur_voxel + B, dds)
         if occupancy_grid[tuple(cur_voxel)]:
             intersect = True
     occupancy_grid[tuple(target_voxel)] = tmp
